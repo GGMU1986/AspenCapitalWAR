b243e039e9e37ef654e59961e4d003b0a4d055fa
ggmu1986 <gt2186@gmail.com>
2022-02-02 10:28:53 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do 
+    resources :players, only: [:update]
+    resources :game, only: [:new]
+  end
+
+  root 'static_pages#root'
 end
